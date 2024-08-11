44145f6f28142b987e6293c04692250605fad95d
YuichiNAGAO <azkgfa03nagao@gmail.com>
2024-08-12 08:48:54 +0900

    modify layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <div className="flex flex-col justify-between w-full h-full min-h-screen">
+        <main className="flex-auto w-full max-w-3xl px-4 py-4 mx-auto sm:px-6 md:px-6 md:py-6">
+          {children}
+        </main>
+      </div>
     </html>
   );
 }
